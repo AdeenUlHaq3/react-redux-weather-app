198baf35fbded4f198cc24c5a16b15d569dc588b
adeenulhaq3 <adeen553@gmail.com>
2018-09-22 13:29:52 +0500

    Exporting Action Creator

--- src/containers/actions.js
+++ src/containers/actions.js
@@ -1,1 +1,11 @@
 const API_KEY = '1da84e4ce8c59a2f9ad109cfdaa130eb';
+const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const Fetch_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather() {
+    return {
+        type: Fetch_WEATHER,
+
+    };
+}
