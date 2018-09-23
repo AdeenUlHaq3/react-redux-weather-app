b3f1d5dc1c7199f58e14c60db5e36ef8e0a3a654
adeenulhaq3 <adeen553@gmail.com>
2018-09-23 09:11:19 +0500

    Adding axios

--- src/containers/actions.js
+++ src/containers/actions.js
@@ -1,11 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '1da84e4ce8c59a2f9ad109cfdaa130eb';
 const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const Fetch_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+    
     return {
         type: Fetch_WEATHER,
-
+        payload: request
     };
 }
