cad4ffcf704f1573488d25f52848c05934bdf724
adeenulhaq3 <adeen553@gmail.com>
2018-09-24 13:27:27 +0500

    Redux Promise Middleware

--- src/AppState.js
+++ src/AppState.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 
+import fetchWeather from './containers/reducer';
+
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  // state: (state = {}) => state
+  weather: fetchWeather
 });
 
 export default rootReducer;
--- src/containers/actions.js
+++ src/containers/actions.js
@@ -8,6 +8,8 @@ export const Fetch_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},pk`;
     const request = axios.get(url);
+    console.log('Request:' ,request);
+    
     
     return {
         type: Fetch_WEATHER,
